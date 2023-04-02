7c06fc9066e4c6c0ccfefd7419c579ee4d6c6b73
Fabian Steinlechner <fasteinlechner@tsn.at>
2023-04-02 13:15:17 +0200

    first_push-fs

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,82 +1,97 @@
-<style>
-@import 'bootstrap';
-@import 'datatables.net-bs5';
-</style>
-
 <template>
-  <div class="container" style="padding:15px">
-    <div class="row">
-      <div class="col-sm">
-        <select v-model="currentNoc">
-          <option v-for="c in countriesNoc">{{ c }}</option>
-        </select>
-      </div>
-      <div class="col-sm">
-        <select v-model="currentEvent">
-          <option v-for="e in events">{{ e }}</option>
-        </select>
-      </div>
+  <div id="app">
+    <div class="cell cell-map">
+      <MapContainer :geojson="geojson" v-on:select="selected = $event"></MapContainer>
     </div>
-    <div class="row" style="padding:15px">
-      <countryComponent></countryComponent>
+    <div class="cell cell-edit">
+      <Edit :geojson="geojson" v-on:change="geojson = $event"></Edit>
     </div>
-
-    <div class="row" style="padding:15px">
-      <eventComponent :currentNoc="currentNoc" :currentEvent="currentEvent"></eventComponent>
+    <div class="cell cell-inspect">
+      <Inspect :feature="selected"></Inspect>
     </div>
-    <div class="row" style="padding:15px">
-      <medallsComponent :currentNoc="currentNoc" :currentEvent="currentEvent"></medallsComponent>
-    </div>
-     
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import medallsComponent from "./components/Component_medals.vue";
-import eventComponent from "./components/Component_event.vue";
-import countryComponent from "./components/Component_country.vue";
-
-
-export default {
-  data() {
-    return {
-      countries: [],
-      countriesNoc: [],
-      events: [],
-      currentEvent: "Alpine Skiing Men's Combined",
-      currentNoc: "AUT"
-    }
-  },
-  methods: {
-    async getContries() {
-      await axios.get(`http://127.0.0.1:5000/regions`)
-        .then(result => this.countries = result.data)
+  import MapContainer from './components/MapContainer.vue'
+  import Edit from './components/Edit.vue'
+  import Inspect from './components/Inspect.vue'
+  export default {
+    name: 'App',
+    components: {
+      Inspect,
+      Edit,
+      MapContainer
     },
-    async getEvents() {
-      await axios.get(`http://127.0.0.1:5000/get_events_by_NOC/${this.currentNoc}`)
-        .then(result => this.events = result.data)
-    }
-  }, async mounted() {
-    await this.getContries()
-    await this.getEvents()
-    this.countries.forEach(element => {
-      this.countriesNoc.push(element.noc)
-    });
+    data: () => ({
+      selected: undefined,
+      geojson: {
+        type: 'Feature',
+        properties: {
+          name: 'default object',
+          quality: 'top'
+        },
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [
+                -27.0703125,
+                43.58039085560784
+              ],
+              [
+                -28.125,
+                23.563987128451217
+              ],
+              [
+                -10.8984375,
+                32.84267363195431
+              ],
+              [
+                -27.0703125,
+                43.58039085560784
+              ]
+            ]
+          ]
+        }
+      }
+    })
+  }
+</script>
 
-  }, watch: {
-    currentNoc() {
-      this.getEvents()
-      e
-    }
-  },
-  components: {
-    medallsComponent,
-    eventComponent,
-    countryComponent
+<style>
+  html, body {
+    margin: 0;
+  }
+  #MyApp {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    height: 100vh;
+    box-sizing: border-box;
+    width: 100%;
   }
 
-}
+  #app {
+    height: 100%;
+    width: 100%;
+    display: grid;
+  }
 
-
-</script>
+  .cell {
+    border-radius: 4px;
+    background-color: lightgrey;
+    height: 100%;
+  }
+  .cell-map {
+    grid-column: 1;
+    grid-row-start: 1;
+    grid-row-end: 3;
+  }
+  .cell-edit {
+    grid-column: 2;
+    grid-row: 1;
+  }
+  .cell-inspect {
+    grid-column: 2;
+    grid-row: 2;
+  }
+</style>
